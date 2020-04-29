812a52e448e06bb3753c063e4d27987e753edabc
cngc4e <cngc4e@gmail.com>
2020-04-29 19:35:28 +0800

    latency.lua: Remove the unnecessary stuff :/

--- latency.lua
+++ latency.lua
@@ -95,45 +95,6 @@ local function init()
         tfm.exec['disable'..v](true)
     end
     system.disableChatCommandDisplay(nil,true)
-    for name in pairs(tfm.get.room.playerList) do eventNewPlayer(name) end
-end
-
---function __eventLoop()
---    for name,p in pairs(tfm.get.room.playerList) do
---        if players[name].is_tracking then
---            local x,y = p.x-1, p.y-2
---            --print(x.." , "..y)
---            ui.addTextArea(1, "", nil, x, y, 4, 4, 0xfc572d, 0xffffff, .5, false)
---            --ui.addTextArea(2,"<J>"..players[name].track_name,nil,x,y,4,4,1,0,0.8,true)
---        end
---    end   
---end
-
-function eventKeyboard(pn, key, down, x, y)
-    if players[pn].is_tracking then
-        x,y = x-1, y-2
-        --print(x.." , "..y)
-        ui.addTextArea(1, "", nil, x, y, 4, 4, 0xfc572d, 0xffffff, .5, false)
-        --ui.addTextArea(2,"<J>"..players[name].track_name,nil,x,y,4,4,1,0,0.8,true)
-    end
-    --eventLoop()
-end
-
-local last_pid = 0
-function eventNewPlayer(pn)
-    last_pid = last_pid + 1
-    players[pn] = {
-        pid = last_pid,
-        is_tracking = false,
-    }
-    tfm.exec.bindKeyboard(pn, 32, true)
-    tfm.exec.bindKeyboard(pn, 32, false)
-end
-
-function eventPlayerLeft(pn)
-    if players[pn] then
-        players[pn].is_tracking = false
-    end
 end
 
 function eventChatCommand(pn, msg)
@@ -142,18 +103,7 @@ function eventChatCommand(pn, msg)
     for arg in msg:gmatch("[^ ]+") do
         args[#args + 1] = arg
     end
-    if args[1] == "track" then
-        if players[pn].is_tracking then
-            players[pn].is_tracking = false
-            ui.removeTextArea(players[pn].pid)
-        else
-            players[pn].track_name = args[2] or pn
-            players[pn].is_tracking = true
-            --eventLoop()
-        end
-    elseif args[1] == 'np' or args[1] == 'map' then
-        tfm.exec.newGame(args[2])
-    elseif args[1] == 'ping' then
+    if args[1] == 'ping' then
         tfm.exec.removeCheese(pn)
         ps:addPlayer(pn, tonumber(args[2]))
     end
