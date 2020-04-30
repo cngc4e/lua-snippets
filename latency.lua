07545ba796bfebd45a1ddecefd64713dd8e48ba1
cngc4e <cngc4e@gmail.com>
2020-04-30 13:25:24 +0800

    latency.lua: remove dependency on metatables

--- latency.lua
+++ latency.lua
@@ -1,11 +1,11 @@
-local PingSystem
+local ping_system
 do
-    PingSystem = { }
-    PingSystem.__index = PingSystem
-
+    local players = {}
+    local packet_received_callback = nil
+    local results_callback = nil
     local send_delay_threshold_ms = 0 -- a delay after which the next packet can be sent again after receiving one. used to prevent race causing cheese not getting removed in time.
 
-    local tests_results = function(numtbl)
+    local function tests_results(numtbl)
         local sum, lowest, highest, cnt = 0, numtbl[1], numtbl[1], numtbl._count
         for i = 1, cnt do
             sum = sum + numtbl[i]
@@ -18,17 +18,9 @@ do
         return { average = sum / cnt, lowest = lowest, highest = highest }
     end
 
-    function PingSystem:new()
-        return setmetatable({
-            players = {},
-            packet_received_callback = nil,
-            results_callback = nil
-        }, self)
-    end
-
-    function PingSystem:addPlayer(pn, test_counts)
+    local function add_player(pn, test_counts)
         test_counts = test_counts or 4
-        self.players[pn] = { 
+        players[pn] = { 
             max_tests_count = test_counts,
             tests = { _count = 0 },
             is_awaiting_packet = false,
@@ -37,22 +29,22 @@ do
         }
     end
 
-    function PingSystem:removePlayer(pn)
-        self.players[pn] = nil
+    local function remove_player(pn)
+        players[pn] = nil
     end
 
-    function PingSystem:setPacketReceivedCallback(cb)
-        self.packet_received_callback = cb
+    local function set_packet_received_callback(cb)
+        packet_received_callback = cb
     end
 
-    function PingSystem:setResultsCallback(cb)
-        self.results_callback = cb
+    local function set_results_callback(cb)
+        results_callback = cb
     end
 
     -- To be hooked on to eventLoop
-    function PingSystem:sendPackets()
+    local function send_packets()
         local now_epoch = os.time()
-        for name, player in pairs(self.players) do
+        for name, player in pairs(players) do
             if not player.is_awaiting_packet then
                 if not player.packet_received_time or now_epoch - player.packet_received_time >= send_delay_threshold_ms then
                     tfm.exec.removeCheese(name)
@@ -65,13 +57,13 @@ do
     end
 
     -- To be hooked on to eventPlayerGetCheese
-    function PingSystem:receivePacket(pn)
-        local player = self.players[pn]
+    local function receive_packet(pn)
+        local player = players[pn]
         if player and player.is_awaiting_packet then
             local now_epoch = os.time()
             local time_ms = (now_epoch - player.packet_sent_time)
-            if self.packet_received_callback then
-                self.packet_received_callback(pn, time_ms)
+            if packet_received_callback then
+                packet_received_callback(pn, time_ms)
             end
             player.packet_received_time = now_epoch
             player.tests[player.tests._count + 1] = time_ms
@@ -79,33 +71,42 @@ do
             player.is_awaiting_packet = false
             if player.tests._count == player.max_tests_count then
                 -- end of test
-                if self.results_callback then
-                    self.results_callback(pn, tests_results(player.tests))
+                if results_callback then
+                    results_callback(pn, tests_results(player.tests))
                 end
-                self:removePlayer(pn)
+                remove_player(pn)
             end
         end
     end
+
+    -- Publicly accessible methods
+    ping_system = {
+        addPlayer = add_player,
+        removePlayer = remove_player,
+        setPacketReceivedCallback = set_packet_received_callback,
+        setResultsCallback = set_results_callback,
+        sendPackets = send_packets,
+        receivePacket = receive_packet
+    }
 end
 
 ----------------------
 --- sample usage of the PingSystem class
 ----------------------
-local ps = PingSystem:new()
-ps:setPacketReceivedCallback(function(pn, time_ms)
+ping_system.setPacketReceivedCallback(function(pn, time_ms)
     print(pn..": "..time_ms.."ms")
 end)
-ps:setResultsCallback(function(pn, result)
+ping_system.setResultsCallback(function(pn, result)
     print(pn.."'s Average: "..result.average.."ms, Lowest: "..result.lowest.."ms, Highest: "..result.highest.."ms")
 end)
 function eventLoop()
-    ps:sendPackets()
+    ping_system.sendPackets()
 end
 function eventPlayerGetCheese(pn)
-    ps:receivePacket(pn)
+    ping_system.receivePacket(pn)
 end
 function eventPlayerLeft(pn)
-    ps:removePlayer(pn)
+    ping_system.removePlayer(pn)
 end
 
 local function init()
@@ -116,10 +117,10 @@ local function init()
 end
 
 local function pFind(target)
-	local ign = string.lower(target or ' ')
-	for name in pairs(tfm.get.room.playerList) do
-		if string.lower(name):find(ign) then return name end
-	end
+    local ign = string.lower(target or ' ')
+    for name in pairs(tfm.get.room.playerList) do
+        if string.lower(name):find(ign) then return name end
+    end
 end
 
 function eventChatCommand(pn, msg)
@@ -138,7 +139,7 @@ function eventChatCommand(pn, msg)
                 tries = tonumber(args[3])
             end
         end
-        ps:addPlayer(target, tries)
+        ping_system.addPlayer(target, tries)
     end
 end
 
