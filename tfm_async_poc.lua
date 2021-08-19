a8e259d10bfa937f2d1d7b2655b9cc3160b448fe
cngc4e <cngc4e@gmail.com>
2021-08-20 03:59:05 +0800

    async: Support proper exception propagation
    and some bug fixed:
    * function stops returning values after nil
    * awaiting a function simultaneously resumes a dead coroutine the 2nd time

--- tfm_async_poc.lua
+++ tfm_async_poc.lua
@@ -4,7 +4,7 @@ IS_DEBUGGING = true
 queue = {}
 function verbose(...)
     if IS_DEBUGGING then
-        print(...)
+        print("[DBG]", ...)
     end
 end
 
@@ -26,20 +26,58 @@ print = function(...)
 end
 
 -- [[ Async/Await Proof of concept ! ]]
+do
 
+--- A unique pointer indicating an error thrown in the coroutine.
 local ERROR_COROUTINE = { error = 0x03 }
+
+--- Handles the resumption of a yielded coroutine.
+--- Returns the coroutine return values, throws in case of error.
+local function afterYield(...)
+    if select(1, ...) == ERROR_COROUTINE then
+        verbose("caught an error from awaiting")
+        error(select(2, ...), 0)
+    end
+    return ...
+end
+
+--- Handles the return values of the yielded coroutine.
+--- Throws in case of error.
+local function afterResume(...)
+    if select(1, ...) ~= true then
+        verbose("caught an error in afterResume")
+        error(select(2, ...), 0)
+    end
+end
+
+--- Handles the return values of the yielded coroutine.
+--- Returns the coroutine return values in succession, otherwise ERROR_COROUTINE plus any error message.
+local function afterResumePropagate(...)
+    local len = select("#", ...)
+    local retvals = {...}
+    if retvals[1] ~= true then
+        return ERROR_COROUTINE, retvals[2]
+    end
+    return table.unpack(retvals, 2, len)
+end
 
 --- async
 --- @generic T
 --- @param fnc T
 --- @return T
-local function async(fnc)
+function async(fnc)
     local t = {}
-    t.coro = coroutine.create(fnc)
+    t.fnc = fnc
     setmetatable(t, {
         __call = function()
+            local coro = coroutine.create(fnc)
             -- no return because not awaited
-            queue[#queue+1] = function() coroutine.resume(t.coro) end
+            queue[#queue+1] = function()
+                assert(coroutine.status(coro) == "suspended",
+                    "Could not resume couroutine that is in state: " .. coroutine.status(coro))
+                -- Any errors thrown from coro are rethrown in this context, since there is no one awaiting.
+                afterResume(coroutine.resume(coro))
+            end
         end
     })
     return t
@@ -49,48 +87,62 @@ end
 --- @generic T
 --- @param t T
 --- @return T
-local function await(t)
+function await(t)
     local this_coroutine, is_main = coroutine.running()
     if not this_coroutine or is_main then
         error("cannot call await without async")
     end
+    local coro = coroutine.create(t.fnc)
     return function(...)
+        local args_len = select("#", ...)
         local args = {...}
         queue[#queue+1] = function()
-            assert(coroutine.status(t.coro) == "suspended",
-                "Could not resume couroutine that is in state: ", coroutine.status(t.coro))
-            local ret = { coroutine.resume(t.coro, table.unpack(args)) }
-            verbose("debug coroutine status", coroutine.status(t.coro))
-            if ret[1] ~= true then
-                verbose("try resume after error")
-                --- @type string
-                local errmsg = ret[2]
-                if type(errmsg) == "string" then
-                    errmsg = errmsg:match("^%S*:?%d*:? ?(.-)$")
-                else
-                    errmsg = "Unknown error thrown in " .. tostring(t.coro)
+            assert(coroutine.status(coro) == "suspended",
+                "Could not resume couroutine that is in state: " .. coroutine.status(coro))
+
+            -- For debugging purposes, we want to log *after* resuming coro and *before* resuming
+            -- this_coroutine. Add a wrapping logger around afterResumePropagate() if debugging.
+            local afterResumePropagate = afterResumePropagate
+            if IS_DEBUGGING then
+                local original_fnc = afterResumePropagate
+                afterResumePropagate = function(...)
+                    local saveRetVals = function (...)
+                        return select("#", ...), {...}
+                    end
+                    local len, saved_retvals = saveRetVals(original_fnc(...))
+                    verbose("debug executing coroutine status", coroutine.status(coro))
+                    return table.unpack(saved_retvals, 1, len)
                 end
-                coroutine.resume(this_coroutine, ERROR_COROUTINE, errmsg)
-            else
-                verbose("try resume", this_coroutine)
-                coroutine.resume(this_coroutine, table.unpack(ret, 2))
             end
+
+            -- Cross-eyed? It's okay me too.
+            -- 1. Resume this_coroutine with the return values of coro
+            -- 2. Any errors thrown from this_coroutine (unhandled) are rethrown in this context per
+            --    default behavior. At this point it's not wrong to say that no one is awaiting.
+            afterResume(
+                coroutine.resume(
+                    this_coroutine,
+                    afterResumePropagate(coroutine.resume(coro, table.unpack(args, 1, args_len)))
+                )
+            )
         end
         verbose("await before", this_coroutine)
-        return coroutine.yield()
+        return afterYield(coroutine.yield())
     end
 end
 
---- async error check
----
---- unfortunately due to the limitations of lua coroutines, we're unable
---- to resume the thread and throw an error immediately. so we have to rely
---- on the fact that the first return value may possibly be an error value.
---- this function checks for that.
---- @param val any # the first return value of the await
---- @return boolean
-local function isAsyncError(val)
-    return val == ERROR_COROUTINE
+end
+
+-- [[ A basic try / catch implementation ]]
+
+--- try catch
+--- @param fnc fun() # The function to execute synchronously.
+--- @param catchFnc fun(e: string) # The function to execute synchronously if `fnc` throws.
+function trycatch(fnc, catchFnc)
+    local status, exception = pcall(fnc)
+    if not status then
+        catchFnc(exception)
+    end
 end
 
 -- [[ Start init ]]
@@ -121,9 +173,11 @@ local async_for_op = async (function ()
 end)
 
 --- test async exception function
-local async_error = async (function()
-    error("thrown error!")
-    return "success apparently?!"
+--- @param throw boolean # whether to throw
+--- @return string
+local async_error = async (function(throw)
+    if throw then error("thrown error!") end
+    return "success !"
 end)
 
 --- the main test !
@@ -133,6 +187,7 @@ local test = async (function ()
     print(v1, v2)
 
     print("now let's count to four !")
+    -- We must create a coroutine container to ensure that the context is saved from yielding.
     local await_container = await (async_for_op)
     local val = await_container()
     while val ~= nil do
@@ -141,11 +196,14 @@ local test = async (function ()
     end
 
     print("and then let's bite the dust...")
-    local r1, e = await (async_error)()
-    if isAsyncError(r1) then
-        -- Equivalent of a 'catch' block
+    local r1
+    trycatch(function ()
+        print(await (async_error)(false))
+        r1 = await (async_error)(true)
+        print(await (async_error)(false))
+    end, function (e)
         r1 = e
-    end
+    end)
 
     print(r1)
 
